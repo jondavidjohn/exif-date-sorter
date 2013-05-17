de069b9e3577ab58222225e15f3f2f229e87d0db
Jonathan Johnson <jon@crowdfavorite.com>
2013-05-16 21:36:20 -0600

    add video move support

--- lib/exif-date-sorter.rb
+++ lib/exif-date-sorter.rb
@@ -7,13 +7,20 @@ class ExifDateSorter
   def initialize(source, target)
     @source = source
     @target = target
+    @video_dir = "#{@target}/Videos"
+    FileUtils.mkdir_p(@video_dir)
   end
 
   def move
-    Dir[@source + '/**/*.{jpg,JPG}'].each do |image|
+    Dir[@source + '/**/*.{jpg,JPG,jpeg,JPEG}'].each do |image|
       target_dir = dir(image)
       FileUtils.mkdir_p(target_dir) unless File.directory? target_dir
       FileUtils.move image, target_dir
+    end
+
+
+    Dir[@source + '/**/*.{mov,MOV,avi,AVI,mp4,MP4}'].each do |video|
+      FileUtils.move video, @video_dir
     end
   end
 
