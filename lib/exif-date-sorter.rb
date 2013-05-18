05b226b41e8aaca1ae81f09aa8cef2cd418500aa
Jonathan Johnson <jon@crowdfavorite.com>
2013-05-17 22:19:56 -0600

    more mods

--- lib/exif-date-sorter.rb
+++ lib/exif-date-sorter.rb
@@ -1,34 +1,52 @@
 require "exif-date-sorter/version"
 require "exifr"
 require "fileutils"
+require "securerandom"
 
 class ExifDateSorter
   def initialize(source, target)
     @source = source
     @target = target
     @video_dir = "#{@target}/Videos"
-    @unsortable_dir = #{@target}/Unsortable"
+    @unsortable_dir = "#{@target}/Unsortable"
     FileUtils.mkdir_p(@video_dir)
     FileUtils.mkdir_p(@unsortable_dir)
   end
 
   def move
     puts "Starting Photo Organization..."
-    Dir[@source + '/**/*.{jpg,JPG,jpeg,JPEG}'].each do |image|
-      target_dir = dir(image)
-      if target_dir
-        puts "  Sorting #{image}"
-        FileUtils.mkdir_p(target_dir) unless File.directory? target_dir
-        FileUtils.move image, target_dir
-      else
-        puts "  Unsortable #{image}"
-        FileUtils.move image, @unsortable_dir
+    Dir[@source + '/*/'].each do |subdir|
+      subdir = File.expand_path subdir
+      puts "  DIR => #{subdir}"
+      Dir[subdir + '/**/*.{jpg,JPG,jpeg,JPEG}'].each do |image|
+        target_dir = dir(image)
+        if target_dir
+          puts "    Image #{File.basename(image)} to #{target_dir}"
+          FileUtils.mkdir_p(target_dir) unless File.directory? target_dir
+          if !File.file? File.join(target_dir, File.basename(image))
+            FileUtils.move image, target_dir
+          else
+            FileUtils.move image, File.join(target_dir, SecureRandom.hex(4) + File.basename(image))
+          end
+        end
+
+        Dir[subdir + '/**/*.{mov,MOV,avi,AVI,mp4,MP4}'].each do |video|
+          puts "    Image #{File.basename(video)} to #{@video_dir}"
+          if !File.file? File.join(@video_dir, File.basename(video))
+            FileUtils.move video, @video_dir
+          else
+            FileUtils.move video, File.join(@video_dir, SecureRandom.hex(4) + File.basename(video))
+          end
+        end
+
+        if Dir.entries(subdir).size
+          if !File.directory? File.join(@unsortable_dir, File.basename(subdir))
+            FileUtils.move subdir, @unsortable_dir
+          else
+            FileUtils.move subdir, File.join(@unsortable_dir, SecureRandom.hex(4) + File.basename(subdir))
+          end
+        end
       end
-    end
-
-
-    Dir[@source + '/**/*.{mov,MOV,avi,AVI,mp4,MP4}'].each do |video|
-      FileUtils.move video, @video_dir
     end
   end
 
